dc051b20a87d75d09b759318bb2c1b9bb872af20
LIBERT Brice <bricel67@gmail.com>
2024-10-30 15:25:43 +0100

    add authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-import {Input} from "@/components/ui/input";
+
+'use client'
+
+import {useCurrent} from "@/features/auth/api/use-current";
+import {useEffect} from "react";
+import {useRouter} from "next/navigation";
+import {useLogout} from "@/features/auth/api/use-logout";
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
+  const router = useRouter()
+  const {data, isLoading} = useCurrent()
+  const { mutate } = useLogout()
+
+  useEffect(() => {
+    if(!data && !isLoading) {
+      router.push('/sign-in')
+    }
+  }, [data]);
+
   return (
     <div>
-      <Input/>
+      Only visible to authorized users.
+      <Button onClick={() => mutate()} >Logout</Button>
     </div>
   );
 }
